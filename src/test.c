4350a2f2200a28502dfd31d9e527f5eeaa5ee66c
TheTheoden <gromovfedor13@gmail.com>
2026-03-05 19:19:34 +0700

    added max test and min test

--- src/test.c
+++ src/test.c
@@ -40,7 +40,7 @@ int simple_test() {
   for (int i = 0; i < case_1_result_cnt; i++) {
     if (!CompareData(case_1_out[i], case_1_result[i])) {
       free(case_1_result);
-      fprintf(stderr, "Results do not match\n");
+      fprintf(stderr, "Data does not match\n");
       return 1;
     }
   }
@@ -50,17 +50,152 @@ int simple_test() {
   return 0;
 }
 
-void max_test() {}
+int max_test() {
+  StatData *a;
+  StatData *b;
+  StatData *c;
+  StatData *out;
+  int a_size, b_size, c_size, out_size;
+
+  a_size = 1e5;
+  a = malloc(sizeof(StatData) * a_size);
+
+  if (!a) {
+    fprintf(stderr, "Failed to allocate first input\n");
+    goto ERROR;
+  }
+
+  for (int i = 0; i < a_size; i++) {
+    a[i].id = 1;
+    a[i].count = 1;
+    a[i].cost = 1.0;
+    a[i].primary = 1;
+    a[i].mode = 0;
+  }
+
+  b_size = 1e5;
+  b = malloc(sizeof(StatData) * b_size);
+
+  if (!b) {
+    fprintf(stderr, "Failed to allocate second input\n");
+    goto ERROR;
+  }
+
+  for (int i = 0; i < b_size; i++) {
+    b[i].id = 2 + i;
+    b[i].count = 1;
+    b[i].cost = 1.0;
+    b[i].primary = 0;
+    b[i].mode = (i % 8);
+  }
+
+  c_size = 1e5 + 1;
+  c = malloc(sizeof(StatData) * c_size);
+
+  if (!c) {
+    fprintf(stderr, "Failed to allocate output array\n");
+    goto ERROR;
+  }
+
+  for (int i = 1; i < c_size; i++) {
+    c[i].id = i + 1;
+    c[i].count = 1;
+    c[i].cost = 1.0;
+    c[i].primary = 0;
+    c[i].mode = (i - 1) % 8;
+  }
+  c[0].id = 1;
+  c[0].count = 1e5;
+  c[0].cost = 100000.0;
+  c[0].primary = 1;
+  c[0].mode = 0;
+
+  StoreDump(a, a_size, "test_data/max_in1");
+  StoreDump(b, b_size, "test_data/max_in2");
+
+  system("./process test_data/max_in1 test_data/max_in2 test_data/max_out");
+
+  out = LoadDump("test_data/max_out", &out_size);
+
+  if (!out) {
+    fprintf(stderr, "Failed to load processed data\n");
+    goto ERROR;
+  }
+
+  if (out_size != c_size) {
+    fprintf(stderr, "Wrong number of insatnces\n");
+    goto ERROR;
+  }
+
+  for (int i = 0; i < c_size; i++) {
+    if (!CompareData(out[i], c[i])) {
+      fprintf(stderr, "Data does not match\n");
+      goto ERROR;
+    }
+  }
+
+  printf("max_test passed\n");
+  free(a);
+  free(b);
+  free(c);
+  free(out);
+  return 0;
+
+ERROR:
+  if (a)
+    free(a);
+  if (b)
+    free(b);
+  if (c)
+    free(c);
+  if (out)
+    free(out);
+
+  return 1;
+}
+
+int min_test() {
+  StatData *a = NULL;
+  StatData *b = NULL;
+  StatData *out;
+  int a_size, b_size, out_size;
+
+  a_size = 0;
+  b_size = 0;
+
+  StoreDump(a, a_size, "test_data/min_in1");
+  StoreDump(b, b_size, "test_data/min_in2");
+
+  system("./process test_data/min_in1 test_data/min_in2 test_data/min_out");
+
+  out = LoadDump("test_data/min_out", &out_size);
+
+  if (out_size != 0) {
+    if (out)
+      free(out);
+    fprintf(stderr, "Wrong number of insatnces\n");
+    return 1;
+  }
+
+  if (out) {
+    free(out);
+  }
+
+  printf("min_test passed\n");
+  return 0;
+}
 
 int main() {
   srand(time(NULL));
   int num_tests = 0;
   num_tests += simple_test();
+  num_tests += max_test();
+  num_tests += min_test();
 
   if (num_tests == 0) {
     printf("All tests passed.\n");
   } else {
-    printf("Some tests have failed.\n");
+    printf("%d of the tests have failed.\n", num_tests);
   }
   return 0;
 }
